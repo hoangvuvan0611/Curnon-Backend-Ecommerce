91c4b9fe3dfa672c23e3a68b13f487f02000d074
Vũ Văn Hoàng <6656485@sv.vnua.edu.vn>
2024-08-05 15:27:51 +0700

    first commit

---
+++ src/main/java/org/vvh/curnonbackendecommerce/CurnonBackendEcommerceApplication.java
@@ -0,0 +1,10 @@
+package org.vvh.curnonbackendecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurnonBackendEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurnonBackendEcommerceApplication.class, args
